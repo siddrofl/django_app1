b6427401534ce52b4ef1f5fb7536e924e2feed4b
siddrofl <murtaza.work404@gmail.com>
2022-06-12 20:37:50 +0500

    user registration page

--- django_app1/settings.py
+++ django_app1/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "blog.apps.BlogConfig",
+    "users.apps.UsersConfig",
+    "crispy_forms",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -120,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
