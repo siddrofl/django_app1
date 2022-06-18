008f8edb73c6222ec0b06df10a02b0208d0ba540
siddrofl <murtaza.work404@gmail.com>
2022-06-18 09:49:52 +0500

    login and registration pages increment

--- django_app1/settings.py
+++ django_app1/settings.py
@@ -126,4 +126,8 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL ='login'
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
